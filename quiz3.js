8d9a5ae8b17a76646e969c435f818b60e4c76eaa
KHOO YIE ZHE <yiezhe1234@gmial.com>
2023-04-11 14:45:15 +0800

    add register and login

--- quiz3.js
+++ quiz3.js
@@ -2,12 +2,50 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.use(express.json())
+let dbUsers=[
+  {   
+      username : "Khoo",
+      password : "112233",
+      name : "Khoo",
+      email : "khoo@student.utem.edu.my"
+  },
+  {   
+      username : "Ho",
+      password : "123456",
+      name : "Ho",
+      email : "ho@student.utem.edu.my"
+  },
+  {   
+      username : "Tan",
+      password : "223344",
+      name : "Tan",
+      email : "tan@student.utem.edu.my"
+  }
+]
 
-app.post('/login', (req, res) => { //req=requesr, res=response
-  console.log(req.body)
-  res.send('Login') 
-})
+function login(reqUsername,reqPassword){
+  let matchUser = dbUsers.find(user => user.username == reqUsername) //=> means array dbUsers will pass every element it find into the variable "user"
+
+  if(!matchUser) return "User not found!"
+
+  if(matchUser.password == reqPassword){
+      return matchUser
+  } 
+  else{
+      return "Invalid password!"
+  }
+}
+
+function register(regUsername,regPassword,regName,regEmail){
+  dbUsers.push( //add the element into array dbUsers
+      {
+          username : regUsername,
+          password : regPassword,
+          name : regName,
+          email : regEmail
+      }
+  )
+}
 
 app.get('/', (req, res) => {
   res.send('404 Not Found') //localhost:300
@@ -17,10 +55,20 @@ app.get('/bye', (req, res) => {
   res.send('Bye 404 Not Found') //localhost:300/bye
 })
 
-app.post('/register', (req, res) => {
-  res.send('Account Created') 
+app.use(express.json())
+app.post('/login', (req, res) => { //req=requesr, res=response
+  console.log(req.body)
+
+  let result = login(req.body.username,req.body.password)
+  res.send(result) 
 })
 
+app.post('/register', (req, res) => {
+  console.log(req.body)
+
+  let result = register(req.body.username,req.body.password,req.body.name,req.body.email)
+  res.send(result) 
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
