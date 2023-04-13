acd3c095c1c9a9b65a5faeec0713e487bdb586df
KHOO YIE ZHE <yiezhe1234@gmial.com>
2023-04-13 15:43:39 +0800

    add token

--- quiz3.js
+++ quiz3.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const jwt = require('jsonwebtoken');
 
 let dbUsers=[
   {   
@@ -47,12 +48,33 @@ function register(regUsername,regPassword,regName,regEmail){
   )
 }
 
+function generateToken(userData){
+  const token = jwt.sign(userData, 'passwordforthetoken',{ expiresIn : 60 }); //userData is the input of the token, 'passwordforthetoken' is the password of the token, expiresIn means the valid time of the token
+  return token;
+}
+
+function verifyToken(req, res, next){
+  let header = req.headers.authorization
+  console.log(header)
+  let token = header.split(' ')[1]//Authorization: Bearer[0] Token[1], split bearer and token to two seperate item
+
+  jwt.verify(token,'passwordforthetoken',function(err,decoded){
+    if(err){
+      res.send("Invalid Token")
+    }
+
+    req.user = decoded
+    next()
+  })
+}
+
 app.get('/', (req, res) => {
   res.send('404 Not Found') //localhost:300
 })
 
-app.get('/bye', (req, res) => {
+app.get('/check',verifyToken, (req, res) => {
   res.send('Bye 404 Not Found') //localhost:300/bye
+
 })
 
 app.use(express.json())
@@ -60,7 +82,8 @@ app.post('/login', (req, res) => { //req=requesr, res=response
   console.log(req.body)
 
   let result = login(req.body.username,req.body.password)
-  res.send(result) 
+  let token = generateToken(result)
+  res.send(token) 
 })
 
 app.post('/register', (req, res) => {
@@ -69,7 +92,6 @@ app.post('/register', (req, res) => {
   let result = register(req.body.username,req.body.password,req.body.name,req.body.email)
   res.send(result) 
 })
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
